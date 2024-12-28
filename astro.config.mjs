4cbb4019601de7980ec1649762fcaaad89d24418
Moisés Huaringa <moiseseduardo.hp@gmail.com>
2024-12-27 22:11:01 -0500

    feat: improvement load ga with partytown lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({ config: { forward: ['dataLayer.push'] } }),
+  ],
   output: 'server',
   adapter: vercel({
     isr: {
