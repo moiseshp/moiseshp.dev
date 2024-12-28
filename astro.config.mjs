d00e016ff83fbd2bf88883d515bafc8468b2b91f
Moisés Huaringa <moiseseduardo.hp@gmail.com>
2024-12-28 00:23:22 -0500

    fix: remove partytown lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import partytown from '@astrojs/partytown';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
-  site: 'https://moiseshp.dev',
+  integrations: [tailwind()],
   output: 'server',
   adapter: vercel({
     isr: {
