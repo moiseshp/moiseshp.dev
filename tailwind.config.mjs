c9ab27fa90771a4b86a3d929ebe87e82f55e7b13
Moisés Huaringa <moiseseduardo.hp@gmail.com>
2024-12-29 00:26:13 -0500

    feat: simplify theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,17 +14,11 @@ export default {
         border: 'hsl(var(--border))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
+        primary: 'hsl(var(--primary))',
+        chip: 'hsl(var(--chip))',
         muted: {
           DEFAULT: 'hsl(var(--muted))',
           foreground: 'hsl(var(--muted-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
         },
       },
     },
